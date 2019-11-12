99a59406f46e3c45f508c85addb991726fa6d045
NaldoRon <49801496+NaldoRon@users.noreply.github.com>
2019-11-12 04:16:53 -0500

    Lab

---
+++ info2180-lab6-master/app.js
@@ -0,0 +1,11 @@
+function searchBox(){
+document.getElementById("search_button").innerHTML
+alert("Search Complete");
+     return getText();
+}
+
+function getText(){
+  document.getElementById(text).value.toLowerCase();
+}
+
+const heroes = [Captain America,Ironman,Spiderman,Captain Marvel,Black Widow,Black Widow,Hawkeye,Black Panther,Thor,Scarlett Witch];
