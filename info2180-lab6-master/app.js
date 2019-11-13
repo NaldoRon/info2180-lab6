f15d2237e63bacbae17246e90cb291c964fa4b99
NaldoRon <49801496+NaldoRon@users.noreply.github.com>
2019-11-12 21:51:52 -0500

    Lab

--- info2180-lab6-master/app.js
+++ info2180-lab6-master/app.js
@@ -1,11 +1,28 @@
-function searchBox(){
-document.getElementById("search_button").innerHTML
-alert("Search Complete");
-     return getText();
-}
+$(document).ready(function(){
+  var xml_button = $('#getJson');
+
+  if (str.length==0){
+  document.getElementById("search_button").innerHTML = ""
+  alert("Search Complete");
+  return;
+ }
+ if (str.length>0){
+   json_button.on('click',searchBox(str){
+   $.ajax("superheroes.php",{
+     method: "GET",
+     dataType: 'json'
+   }).done(function(response){
+     var hero_name = response.$(superheroes);
+     $('#results').append("<ul><ul>");
+     $(hero_name).each(function()){
+     $("#results ul").append("<li><h3>"+this.id+this.name+('aka'+this.alias+)+this.biography+"</p></li>");
+    });
+  }).fail(function(){
+     alert(result+""+"is not an Avenger.");
+   });
+ });
+});
 
 function getText(){
   document.getElementById(text).value.toLowerCase();
 }
-
-const heroes = [Captain America,Ironman,Spiderman,Captain Marvel,Black Widow,Black Widow,Hawkeye,Black Panther,Thor,Scarlett Witch];
